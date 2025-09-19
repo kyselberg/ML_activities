3b11bc199b4684088871fcf9ae1c2215b20e02ea
Illia Kyselov <69676320+kyselberg@users.noreply.github.com>
2025-09-19 11:03:27 +0300

    Add iris scatter matrix visualization and KNN classifier implementation
    - Introduced a new scatter matrix visualization for the Iris dataset, saved as 'iris_scatter_matrix.png'.
    - Updated 'irises.py' to include KNeighborsClassifier for model training and evaluation.
    - Added necessary imports for data manipulation and visualization.
    - Updated requirements.txt to include mglearn dependency.

--- book/irises/irises.py
+++ book/irises/irises.py
@@ -1,22 +1,21 @@
 from sklearn.datasets import load_iris
 iris_dataset = load_iris()
-print(iris_dataset.keys())
-# print(iris_dataset['DESCR'])
-print(iris_dataset['feature_names'])
-# print(iris_dataset['target'])
-# print(iris_dataset['target_names'])
-# print(iris_dataset['data'])
-# print(iris_dataset['data'].shape)
 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0) # try to increase random_state to see if the results are different
 
-# from sklearn.model_selection import train_test_split
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+import mglearn
+import matplotlib.pyplot as plt
 
-# iris = load_iris()
-# X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3, random_state=42)
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15,15), marker='0', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+plt.savefig('iris_scatter_matrix.png')
 
-# clf = DecisionTreeClassifier()
-# clf.fit(X_train, y_train)
+from sklearn.neighbors import KNeighborsClassifier
 
-# print(accuracy_score(y_test, clf.predict(X_test)))
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+
+print("Test set score: {:.2f}".format(knn.score(X_test, y_test)))
